83bead0c51d908f3bb816586d2773054d931e3fa
Malay-134 <prajapatimalay1343@gmail.com>
2024-08-22 15:59:42 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} />
+          <p>
+            Lorem ipsum is simply dummy text of the printing and typesetting
+            industry
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>Get in touch</h2>
+          <ul>
+            <li>+91-485-544-4565</li>
+            <li>contact@food.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2022 Food.com. All rights reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
